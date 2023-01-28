3c54955296a0a55ef1d2aa93af19eb546206b5f7
akasurrahman <akasur.rahman24@gmail.com>
2023-01-28 20:04:39 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// Practice Problem-1(a):
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// fruits[1] = 'Mango';
+// console.log(fruits);
+
+// Practice Problem-1(b):
+// var fruits = ['Apple', 'Banana', 'Orange'];
+// fruits.pop();
+// fruits.push('Watermelon');
+// console.log(fruits);
+
+// Practice Problem-2:
+// var grade = 23;
+// if(grade>=80){
+//     console.log('Your Grade is: A');
+// }
+// else if(grade>=60){
+//     console.log('Your Grade is: B');
+// }
+// else if(grade>=50){
+//     console.log('Your Grade is: C');
+// }
+// else if(grade>=40){
+//     console.log('Your Grade is: D');
+// }
+// else if(grade<=39){
+//     console.log('Failed');
+// }
+// else{
+//         console.log('Your result not found!')
+// }
+
+// Practice Problem-3:
